68acc88e8268f878d46e634d2413a12b2b285c19
maxim100000 <105789888+maxim100000@users.noreply.github.com>
2025-12-28 14:07:54 +0700

    Mount Svelte app to shadow DOM instead of container
    Change the mount target for the Svelte app to a shadow DOM.

--- src/content/main.ts
+++ src/content/main.ts
@@ -1,17 +1,22 @@
+import css from '@/content/style.css?inline'
 import { mount } from 'svelte'
 import App from './views/App.svelte'
 
-console.log('[CRXJS] Hello world from content script!')
-
-/**
- * Mount the Svelte app to the DOM.
- */
 function mountApp() {
+  if (document.querySelector('#crxjs-app')) return
   const container = document.createElement('div')
   container.id = 'crxjs-app'
+  container.style.position = 'fixed'
+ container.style.zIndex='3434676567'
+  container.style.bottom='0'
+  container.style.right='0'
   document.body.appendChild(container)
+  let shadow = container.attachShadow({mode: "open"})
+  let style = document.createElement('style')
+  style.textContent = css
+  shadow.appendChild(style)
   mount(App, {
-    target: container,
+    target: shadow,
   })
 }
 
